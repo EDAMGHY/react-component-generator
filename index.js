edb1273d8cf2feb5ee6f8c2a1ab37f175c36341b
Abdellah DAMRI <damriabdellah@gmail.com>
2022-02-25 15:48:36 +0100

    Adding exception for the root dir

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
 const { program } = require('commander');
 const chalk = require('chalk');
-const fs = require('fs');
 const createFileFolder = require('./utils');
 const { createSpinner } = require('nanospinner');
+const package = require('./package.json');
 
 const log = console.log;
-program.version('1.1.2').description('A React Component Generator').parse();
 
+program
+  .version(package.version)
+  .description('A React Component Generator')
+  .parse();
+const path = process.cwd();
+const spinner = createSpinner(chalk.yellow('Creating File...')).start();
 if (program.args.length === 0) {
   setTimeout(() => {
     log(chalk.redBright('Please Enter a Folder(s)/File Name !'));
@@ -15,25 +20,32 @@ if (program.args.length === 0) {
 } else if (program.args.length === 1) {
   let fileName = program.args[0];
   fileName = fileName[0].toUpperCase() + fileName.slice(1);
-  const spinner = createSpinner(chalk.yellow('Creating File...')).start();
-  setTimeout(() => {
-    createFileFolder(fileName);
-    spinner.success({
-      text: chalk.green(`${fileName} Component Created...`),
-    });
-  }, 2000);
+  if (path.includes('src') || path.includes('public')) {
+    log(chalk.red('You should be in the root directory of your project!'));
+  } else {
+    setTimeout(() => {
+      createFileFolder(fileName);
+      spinner.success({
+        text: chalk.green(`${fileName} Component Created...`),
+      });
+    }, 2000);
+  }
 } else {
   try {
     const folderName = program.args[0];
     let fileName = program.args[1];
     fileName = fileName[0].toUpperCase() + fileName.slice(1);
-    const spinner = createSpinner(chalk.yellow('Creating File...')).start();
-    setTimeout(() => {
-      createFileFolder(fileName, folderName, true);
-      spinner.success({
-        text: chalk.green(`${fileName} Component Created...`),
-      });
-    }, 2000);
+
+    if (path.includes('src') || path.includes('public')) {
+      log(chalk.red('You should be in the root directory of your project!'));
+    } else {
+      setTimeout(() => {
+        createFileFolder(fileName, folderName, true);
+        spinner.success({
+          text: chalk.green(`${fileName} Component Created...`),
+        });
+      }, 2000);
+    }
   } catch (err) {
     log(chalk.bgRed.white(err.message));
   }
